d482eac2e3379b91573e7ea439bcbe0faee68dc0
Santoshrt999 <gsr.teja@gmail.com>
2018-01-01 09:09:45 -0600

    Added CK EDitor

--- models/Idea.js
+++ models/Idea.js
@@ -1,26 +1,24 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-//creating schema
+// Create Schema
 const IdeaSchema = new Schema({
-    title:{
-        type: String,
-        requried: true
-    },
-    
-        details:{
-            type: String,
-            required: true
-        },
-        user:{
-            type: String,
-            required: true
-        },
-        date:{
-            type:Date,
-            default:Date.now
-        }
-    
+  title:{
+    type: String,
+    required: true
+  },
+  details:{
+    type: String,
+    required: true
+  },
+  user:{
+    type: String,
+    required:true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
 });
 
 mongoose.model('ideas', IdeaSchema);
